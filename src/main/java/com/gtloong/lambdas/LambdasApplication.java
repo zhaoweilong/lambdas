636f5e94f7a53e3de5222052759fd2e9357f590d
ZhaoWeilong <zhaoweilong0220@163.com>
2019-06-21 09:13:18 +0800

    项目创建

---
+++ src/main/java/com/gtloong/lambdas/LambdasApplication.java
@@ -0,0 +1,13 @@
+package com.gtloong.lambdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdasApplication.class, args);
+    }
+
+}
